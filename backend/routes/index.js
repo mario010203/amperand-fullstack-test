a01abc7ab750706d4f1b392afa9101cb9db32984
mario010203 <mariojose1490@hotmail.com>
2020-04-28 19:49:49 +0200

    batteries percentages done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const _ = require("lodash");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -17,6 +18,9 @@ const lime = axios.create({
   withCredentials: true,
 });
 
+/*BIRD*/
+
+//bird - all
 router.get("/bird", async (req, res, next) => {
   try {
     const birdRes = await bird.get();
@@ -26,10 +30,38 @@ router.get("/bird", async (req, res, next) => {
   }
 });
 
+//bird - batterylevel
+router.get("/bird/battery", async (req, res, next) => {
+  try {
+    const birdRes = await bird.get();
+    return res.json(_.pick(birdRes.data.birds[0], "battery_level"));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/*LIME*/
+
+//lime - all
 router.get("/lime", async (req, res, next) => {
   try {
     const limeRes = await lime.get();
     return res.json(limeRes.data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//lime - batterylevel
+router.get("/lime/battery", async (req, res, next) => {
+  try {
+    const limeRes = await lime.get();
+    return res.json(
+      _.pick(
+        limeRes.data.data.attributes.vehicles[0].attributes,
+        "battery_percentage"
+      )
+    );
   } catch (err) {
     console.log(err);
   }
